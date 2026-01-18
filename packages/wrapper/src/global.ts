4942588f02866ad02aa6c3bd7fe121ff07663eff
falkura <vlad.trollon@gmail.com>
2026-01-18 15:40:08 +0100

    [wrapper] global script added

---
+++ packages/wrapper/src/global.ts
@@ -0,0 +1,7 @@
+/**
+ * This script will be included in each game and a wrapper
+ */
+
+import Engine from "@falkura-pet/engine";
+
+console.log(Engine);
--- packages/wrapper/src/index.ts
+++ packages/wrapper/src/index.ts
@@ -1,5 +1,4 @@
 import logoPNG from "./../assets/logo.png";
-// import { GamesList } from "./gamesLocal";
 
 const wrapperRoot = window["wrapper"] as HTMLDivElement;
 
@@ -9,29 +8,9 @@ png.height = 100;
 png.src = logoPNG;
 wrapperRoot.appendChild(png);
 
-const btn = document.createElement("button");
-btn.innerText = "Load Game 1";
-btn.onclick = () => {
-  wrapperRoot.removeChild(btn);
-
-  globalThis.loadGame1();
-};
-wrapperRoot.appendChild(btn);
+const a = document.createElement("a");
+a.href = "game1";
+a.text = "Game 1";
+wrapperRoot.appendChild(a);
 
 console.log("Wrapper Loaded Successfully");
-
-globalThis.loadGame = loadGame;
-globalThis.loadGame1 = async () => {
-  const game_module = await loadGame("/games/gameOne");
-  const gameInstance = new game_module.Game();
-
-  return gameInstance;
-};
-
-export async function loadGame(game: string) {
-  // if (import.meta.env.DEV) {
-  //   return GamesList[game]();
-  // }
-
-  return await import(`../../../dist/games/template`);
-}
