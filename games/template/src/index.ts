59f57311527af6bfaa8bca6eee069908a73ba7bb
falkura <vlad.trollon@gmail.com>
2026-01-18 15:41:35 +0100

    [template] engine loading checker added

--- games/template/src/index.ts
+++ games/template/src/index.ts
@@ -1,6 +1,6 @@
 import { Application } from "pixi.js";
 import config from "./config.json";
-
+import Engine from "@falkura-pet/engine";
 // export class Game extends BaseGame {
 //   constructor() {
 //     super(config, import.meta.url);
@@ -9,6 +9,7 @@ import config from "./config.json";
 const app = new Application();
 console.log("Template Loaded Successfully");
 
+console.log(Engine);
 export class Game {
   app = app;
 }
