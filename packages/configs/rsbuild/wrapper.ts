0958de565383b1c71e0610ee54de34e9a35c68c7
falkura <vlad.trollon@gmail.com>
2026-01-18 15:41:11 +0100

    [wrapper] router and engine loading added

--- packages/configs/rsbuild/wrapper.ts
+++ packages/configs/rsbuild/wrapper.ts
@@ -2,18 +2,31 @@ import { defineConfig, RsbuildConfig } from "@rsbuild/core";
 import path from "path";
 
 const root = path.resolve(__dirname, "../../../");
+const distPath = path.resolve(root, "dist");
+const gamesRoot = path.join(distPath, "games");
 
 export const create = (dirname?: string) =>
   defineConfig({
-    source: {},
-    dev: {
-      port: 3000,
+    source: {
+      entry: {
+        game1: ["./src/global.ts", path.join(gamesRoot, "template")],
+        index: ["./src/global.ts", "./src/index.ts"],
+      },
     },
     html: {
       template: "./public/index.html",
+      inject: "body",
+    },
+    server: {
+      port: 3000,
     },
     output: {
-      distPath: path.resolve(root, "dist"),
+      distPath: distPath,
       cleanDistPath: false,
+    },
+    resolve: {
+      alias: {
+        "@falkura-pet/engine": path.join(root, "packages/engine/dist"),
+      },
     },
   } as RsbuildConfig);
