0f56ced0f626fa7f77b44b44eee674936b121e9c
falkura <vlad.trollon@gmail.com>
2026-01-18 17:44:40 +0100

    [wrapper] refactor

--- packages/wrapper/rsbuild.config.ts
+++ packages/wrapper/rsbuild.config.ts
@@ -1,3 +1,41 @@
-import { create } from "@falkura-pet/configs/rsbuild/wrapper.ts";
+import { defineConfig, RsbuildConfig } from "@rsbuild/core";
+import path from "path";
+import config from "../../config.json";
 
-export default create();
+const root = path.resolve(__dirname, "../../");
+const distPath = path.resolve(root, "dist");
+const gamesRoot = path.join(distPath, "games");
+const globalScript = "./src/global.ts";
+
+const games = Object.values(config.games.gamesList)
+  .filter(({ enabled }) => enabled !== false)
+  .map((game) => {
+    const route = game.route || game.path;
+
+    return [route, [globalScript, path.join(gamesRoot, game.path)]];
+  });
+
+export default defineConfig({
+  source: {
+    entry: {
+      ...Object.fromEntries(games),
+      index: [globalScript, "./src/index.ts"],
+    },
+  },
+  html: {
+    template: "./public/index.html",
+    inject: "body",
+  },
+  server: {
+    port: 3000,
+  },
+  output: {
+    distPath: distPath,
+    cleanDistPath: false,
+  },
+  resolve: {
+    alias: {
+      "@falkura-pet/engine": path.join(root, "packages/engine/dist"),
+    },
+  },
+} as RsbuildConfig);
--- packages/wrapper/src/index.ts
+++ packages/wrapper/src/index.ts
@@ -1,16 +1,14 @@
 import logoPNG from "./../assets/logo.png";
 
-const wrapperRoot = window["wrapper"] as HTMLDivElement;
-
 const png = document.createElement("img");
 png.width = 100;
 png.height = 100;
 png.src = logoPNG;
-wrapperRoot.appendChild(png);
+root.appendChild(png);
 
 const a = document.createElement("a");
 a.href = "game1";
 a.text = "Game 1";
-wrapperRoot.appendChild(a);
+root.appendChild(a);
 
 console.log("Wrapper Loaded Successfully");
